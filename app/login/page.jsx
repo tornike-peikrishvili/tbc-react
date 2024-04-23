0ad66fcb2b89f6d5a92a5c2f2037e51fd37ace6e
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-04-23 19:40:24 +0400

    Undo test changes

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,7 @@ async function LoginPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-80">
-        <h2 className="text-3xl font-bold mb-4 text-center">Loginie</h2>
+        <h2 className="text-3xl font-bold mb-4 text-center">Login</h2>
         <LoginForm />
       </div>
     </div>
